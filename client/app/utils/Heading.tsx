77b3d42d9244acf42cc91578c71088f9d79e9223
dinhgiaandev <dinhgiaanforwork@gmail.com>
2024-10-12 14:08:12 +0700

    set favicon for website

--- client/app/utils/Heading.tsx
+++ client/app/utils/Heading.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 interface IHeadProps {
@@ -10,10 +11,13 @@ const Heading = (props: IHeadProps) => {
     const { title, description, keywords } = props;
     return (
         <>
+            <head>
+                <link rel='icon' href='/assets/favicon.ico' type='image/x-icon' />
+            </head>
             <title>{title}</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="viewport" content={description} />
-            <meta name="viewport" content={keywords} />
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords} />
         </>
     );
 };
